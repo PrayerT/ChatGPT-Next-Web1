55cc2c04c9aa3f08ce014cd58b6b24543db41fe9
PrayerT <63850261+PrayerT@users.noreply.github.com>
2024-02-16 21:59:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Prayer's Chat 4.0",
+  description: "自用，持续更新中",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "Prayer's Chat 4.0",
     statusBarStyle: "default",
   },
 };
