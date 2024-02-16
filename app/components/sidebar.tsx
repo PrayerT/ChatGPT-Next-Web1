6937ed7cf574cc066153181d8deafe813ad738e4
PrayerT <63850261+PrayerT@users.noreply.github.com>
2024-02-16 22:49:36 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Prayer's ChatGPT 4.0
+          Prayer&apos;s ChatGPT 4.0
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Constantly evolving！
